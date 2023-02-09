52f163d61ea7aeb9705063ce0f2afe5c10f6ecaf
Thiagarajan_Loganath <Thiagarajan_Loganath@EPINBANW03CC>
2023-02-09 18:57:26 +0530

    Sample

---
+++ AutomationPrep/src/com/java/Constructors.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class Constructors {
+
+	Constructors(){
+		
+		System.out.println("Constuctor");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Constructors cons = new Constructors();
+	}
+
+}
